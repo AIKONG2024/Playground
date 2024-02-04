40245d6d28498782b60f4ee5ce2123b9fed0b7aa
AIKONG2024 <aikong2024@gmail.com>
2024-02-04 17:07:48 +0900

    - fix last

--- competition/dacon/dechul/dacon_dechul_last.py
+++ competition/dacon/dechul/dacon_dechul_last.py
@@ -56,13 +56,9 @@ submission_csv = pd.read_csv(path + "sample_submission.csv")
 lbe = LabelEncoder()
 # 주택소유상태
 train_csv["주택소유상태"] = lbe.fit_transform(train_csv["주택소유상태"])
-if "ANY" not in lbe.classes_:
-    lbe.classes_ = np.append(lbe.classes_, "ANY")
 test_csv["주택소유상태"] = lbe.transform(test_csv["주택소유상태"])
 # 대출목적
 train_csv["대출목적"] = lbe.fit_transform(train_csv["대출목적"])
-if "결혼" not in lbe.classes_:
-    lbe.classes_ = np.append(lbe.classes_, "결혼")
 test_csv["대출목적"] = lbe.transform(test_csv["대출목적"])
 # 근로기간
 train_csv["근로기간"] = lbe.fit_transform(train_csv["근로기간"])
