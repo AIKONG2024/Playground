a4df35731b1552e6022e478543f7dfb10448845a
AIKONG2024 <aikong2024@gmail.com>
2024-04-22 10:49:32 +0900

    dacon bird

--- competition/dacon/bird/inception_resnet_v2_finetune.py
+++ competition/dacon/bird/inception_resnet_v2_finetune.py
@@ -18,9 +18,9 @@ def combined_generator(gen1, gen2):
             x2, y2 = next(gen2)
             yield x2, y2
 # Set random seed for reproducibility
-SEED = 42
+SEED = 1234
 IMAGE_SIZE = (224,224)
-FILTERS = 8
+FILTERS = 16
 BATCH_SIZE = 32
 np.random.seed(SEED)
 tf.random.set_seed(SEED)
